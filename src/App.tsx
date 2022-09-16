f51c6477efe2931ee5c699bf004eebbb0082ac5a
Nieraa <timeboy3@hotmail.com>
2022-09-16 16:59:33 +0700

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { SideNavbar } from './components/sideNavbar/SideNavbar';
-import { AppBar } from './components/appBar/AppBar';
-import { Main } from './components/main/Main';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Projects from './pages/Projects';
+import Planner from './pages/Planner';
+import Realtime from './pages/Realtime';
+import History from './pages/History';
 
 function App() {
+
   return (
-    <div className="App">
-      <AppBar />
-      <SideNavbar items={[]} />
-      <Main />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Projects />}
+      />
+      <Route
+        path="/:projectId/planner"
+        element={<Planner />}
+      />
+      <Route
+        path="/:projectId/realtime"
+        element={<Realtime />}
+      />
+      <Route
+        path="/:projectId/history"
+        element={<History />}
+      />
+    </Routes>
   );
 }
 
---
+++ src/pages/History.tsx
@@ -0,0 +1,26 @@
+import { SideNavbar } from '../components/sideNavbar/SideNavbar';
+import { AppBar } from '../components/appBar/AppBar';
+import { Main } from '../components/main/Main';
+
+function History() {
+  const items = [
+    { title: "Project 1", url: "" },
+    { title: "Project 2", url: "" },
+    { title: "Project 3", url: "" },
+    { title: "Project 4", url: "" },
+    { title: "Project 5", url: "" },
+    { title: "Project 6", url: "" },
+    { title: "Project 7", url: "" },
+    { title: "Project 8", url: "" },
+  ]
+
+  return (
+    <div>
+      <AppBar />
+      <SideNavbar items={items} />
+      <Main />
+    </div>
+  );
+}
+
+export default History;
