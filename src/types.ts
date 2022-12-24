9ebfc9e2029f0a3ed0c570fc1a98db6706f1e7f5
Nieraa <timeboy3@hotmail.com>
2022-12-24 23:53:30 +0700

    assign type to anchor,tag and change project type

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,32 @@
 export interface Project {
   id: string,
   projectName: string,
-  imgUrl?: string,
+  imgUrl: string,
+  l: number,
+  w: number,
 }
 
 export interface PassAndUpdateProjects {
   projects: Project[];
   setProjects: (projects: Project[]) => void;
+}
+
+export interface AnchorType {
+  id: string,
+  name: string,
+  ipAddress: string,
+  x: number,
+  y: number,
+  networkSsid: string,
+  networkColor: string,
+}
+
+export interface TagType {
+  id: string,
+  name: string,
+  ipAddress: string,
+  x: number,
+  y: number,
+  networkSsid: string,
+  networkColor: string,
 }
