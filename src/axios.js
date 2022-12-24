02c2ac94fd8bc0af3bde4ebd8742552edb3147e8
Nieraa <timeboy3@hotmail.com>
2022-12-24 23:52:18 +0700

    update base url for axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: (process.env.NODE_ENV === "development") ? "http://localhost:5000" : "serverUrl",
 });
 
 export default instance;
