07eb7f8d5c8aa4033ea9ad3044a8f34685376d3e
Nieraa <timeboy3@hotmail.com>
2022-12-26 20:30:37 +0700

    define new function about project and hardware

--- src/services/ProjectsService.ts
+++ src/services/ProjectsService.ts
@@ -1,14 +1,127 @@
+import { useNavigate } from "react-router-dom";
 import axios from "../axios";
-import { Project } from "../types";
+import { Hardware, Project } from "../types";
 
-export function getProjects(setProjects: (projects: Project[]) => void) {
+export function getProjects(
+  setProjects: (projects: Project[]) => void,
+  createId?: string,
+): void {
   axios
-    .get(`/projects`)
+    .get("/projects")
     .then((response) => {
-      console.log(response.data);
       setProjects(response.data);
+      if (createId) {
+        const navigate = useNavigate();
+        navigate(`/${createId}/planner`);
+      }
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(() => {
     });
+}
+
+export function createProject(
+  projectData: {
+    projectName: string,
+    imgUrl: string,
+    l: number,
+    w: number,
+  },
+  setProjects: (projects: Project[]) => void,
+): void {
+  axios
+    .post("/projects", projectData)
+    .then((response) => {
+      getProjects(setProjects, response.data.id);
+    })
+    .catch(() => {
+      alert("Create failed");
+    })
+}
+
+export function getHardwares(
+  projectId: string,
+  hardwareType: string,
+  setHardwares: (hardware: Hardware[]) => void,
+  hasNewColor: boolean,
+  setColors?: (colors: string[]) => void,
+  setGroup?: (group: number) => void,
+  setNetworkSsids?: (networkSsids: string[]) => void
+): void {
+  axios
+    .get(`/projects/${projectId}/${hardwareType}s`)
+    .then((response) => {
+      setHardwares(response.data);
+      if (hasNewColor && setColors && setGroup && setNetworkSsids) {
+        getColors(projectId, setColors, setGroup);
+        getNetworkSsids(projectId, setNetworkSsids);
+      }
+    })
+    .catch(() => {
+    });
+}
+
+export function createHardware(
+  projectId: string,
+  hardwareType: string,
+  setHardwares: (hardware: Hardware[]) => void,
+  hardwareData: {
+    projectId: string,
+    name: string,
+    ipAddress: string,
+    x: number,
+    y: number,
+    networkSsid: string,
+    networkColor: string,
+  },
+  hasNewColor: boolean,
+  setColors: (colors: string[]) => void,
+  setGroup: (group: number) => void,
+  setNetworkSsids: (networkSsids: string[]) => void,
+  handleCloseDialog: () => void
+): void {
+  axios
+    .post(`/projects/${projectId}/${hardwareType}s`, hardwareData)
+    .then(() => {
+      getHardwares(
+        projectId,
+        hardwareType,
+        setHardwares,
+        hasNewColor,
+        setColors,
+        setGroup,
+        setNetworkSsids
+      );
+      handleCloseDialog()
+    })
+    .catch(() => {
+      alert("Create failed");
+    });
+}
+
+export function getColors(
+  projectId: string,
+  setColors: (colors: string[]) => void,
+  setGroup: (group: number) => void
+): void {
+  axios
+    .get(`/projects/${projectId}/colors`)
+    .then((response) => {
+      setColors(response.data);
+      setGroup(response.data.length + 1);
+    })
+    .catch(() => {
+    })
+}
+
+export function getNetworkSsids(
+  projectId: string,
+  setNetworkSsids: (networkSsids: string[]) => void,
+): void {
+  axios
+    .get(`/projects/${projectId}/networkSsids`)
+    .then((response) => {
+      setNetworkSsids(response.data);
+    })
+    .catch(() => {
+    })
 }
