d2266e15de494407d993c0fce031c8d7849cf496
Nieraa <timeboy3@hotmail.com>
2023-01-19 15:59:20 +0700

    change baseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: (process.env.NODE_ENV === "development") ? "http://localhost:5000" : "serverUrl",
+  baseURL: "http://localhost:5000/indoor-localization-syst-30486/us-central1/api",
 });
 
 export default instance;
