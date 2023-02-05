c02dcf79272a32b1b881de3d59f003d5e91531b8
Nieraa <timeboy3@hotmail.com>
2023-02-06 00:07:47 +0700

    remove unused import

--- src/components/sideNavbar/SideNavbar.style.ts
+++ src/components/sideNavbar/SideNavbar.style.ts
@@ -1,6 +1,5 @@
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
-import { maxLayout } from "../../utils/breakpoint";
 import '../../theme/Theme.css';
 
 export const Tint = styled.div<{ collapseNavbar: boolean }>`
