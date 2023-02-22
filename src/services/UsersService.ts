7d8d13641b118193bba3378a444aeea405c99967
Nieraa <timeboy3@hotmail.com>
2023-02-22 20:53:23 +0700

    create UsersService

---
+++ src/services/UsersService.ts
@@ -0,0 +1,42 @@
+import axios from "../axios";
+import { SignInData, SignUpData, Username } from "../types";
+
+export async function getUsernames(): Promise<Username[]> {
+  return await axios
+    .get("usernames")
+    .then((response) => {
+      return response.data;
+    })
+    .catch(() => {
+      return [];
+    })
+}
+
+export async function SignIn(
+  signInData: SignInData,
+  handleSignIn: (success: boolean) => void
+) {
+  await axios
+    .post("auth/signin", signInData)
+    .then((response) => {
+      localStorage.setItem("accessToken", response.data.accessToken);
+      handleSignIn(true);
+    })
+    .catch(() => {
+      handleSignIn(false);
+    })
+}
+
+export async function SignUp(
+  signUpData: SignUpData,
+  handleSignUp: (success: boolean) => void
+): Promise<void> {
+  await axios
+    .post("signup", signUpData)
+    .then(() => {
+      handleSignUp(true);
+    })
+    .catch(() => {
+      handleSignUp(false);
+    });
+}
