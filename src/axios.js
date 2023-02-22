ba63cfdbc1ca6028bfa7e80c9636b3ace71746f9
Nieraa <timeboy3@hotmail.com>
2023-02-23 01:33:29 +0700

    add baseUrl for development

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-indoor-localization-syst-30486.cloudfunctions.net/api",
+  // baseURL: "https://us-central1-indoor-localization-syst-30486.cloudfunctions.net/api",
+  baseURL: "http://127.0.0.1:5001/indoor-localization-syst-30486/us-central1/api",
 });
 
 export default instance;
