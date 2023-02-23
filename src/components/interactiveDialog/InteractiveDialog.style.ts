20e7c87166bfd23c70431fd860c84fa766b6ed6c
Nieraa <timeboy3@hotmail.com>
2023-02-23 16:04:37 +0700

    create InteractiveDialog component

---
+++ src/components/interactiveDialog/InteractiveDialog.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  text-align: center;
+`;
+
+export const Title = styled.div`
+  color: var(--primary);
+  font-size: var(--sm-heading);
+  font-weight: bold;
+`;
+
+export const Detail = styled.div`
+  font-size: var(--md-text);
+  margin-top: 20px;
+`;
---
+++ src/components/interactiveDialog/InteractiveDialog.tsx
@@ -0,0 +1,64 @@
+import {
+  ContentWrapper,
+  Title,
+  Detail,
+} from "./InteractiveDialog.style";
+import { CancelButton } from "../../Styles/Styles.style";
+import {
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+
+interface InteractiveDialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  signOut: () => void;
+}
+
+function InteractiveDialog(props: InteractiveDialogProps) {
+  const {
+    open,
+    setOpen,
+    signOut
+  } = props;
+
+  function handleClose(): void {
+    setOpen(false);
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      fullWidth
+    >
+      <DialogContent>
+        <Title>
+          Are you sure?
+        </Title>
+        <Detail>
+          Do you really want to sign out?
+        </Detail>
+      </DialogContent>
+      <DialogActions>
+        <CancelButton
+          variant="contained"
+          onClick={handleClose}
+        >
+          Cancel
+        </CancelButton>
+        <Button
+          color="error"
+          variant="contained"
+          onClick={signOut}
+        >
+          Sign out
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default InteractiveDialog;
